173f3fd1bef8283d4749687211a5fc75fe1a79d1
Joshua Pimentel <josh@melodyarc.com>
2021-11-23 14:59:27 -0800

    Init commit

---
+++ notes-app/app - Copy (2).js
@@ -0,0 +1,67 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// customize yargs version
+yargs.version('1.1.0')
+
+//  add, remove, read, list
+
+// Create add command
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler (argv) {
+    notes.addNote(argv.title, argv.body)
+    
+  }
+})
+
+// crate remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+      }
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+// crate list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all notes')
+  }
+})
+
+// crate read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+     console.log('Reading the notes')
+  }
+})
+
+yargs.parse()
---
+++ notes-app/app - Copy.js
@@ -0,0 +1,29 @@
+// const fs = require('fs')
+const chalk = require('chalk');
+
+const greenMsg = chalk.green('Success!')
+
+console.log(chalk.bold.green('Success!'))
+
+// const getNotes = require('./notes.js')
+
+// const msg = getNotes()
+
+//  console.log(msg)
+
+
+
+
+
+
+
+
+// fs.writeFileSync('notes.txt', 'My name is Joshua,')
+
+// fs.appendFileSync('notes.txt', ' and I am learning to code with Note.js!');
+
+/* Challenge: append a message to notes.txt
+1. Use appendFileSync to append to the file
+2. Run the script
+3. Check your work by opening the file and append text.
+*/
