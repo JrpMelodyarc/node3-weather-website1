482a490c999a926580c79ec65c004f54b66b618e
Joshua Pimentel <josh@melodyarc.com>
2021-11-24 15:10:14 -0800

    make it best

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.' + ' With a ' + body.current.wind_speed + ' mph wind speed and a' + body.current.precip + ' Chance of rain')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.' + ' With a ' + body.current.wind_speed + ' mph wind speed and a' + body.current.precip + '% Chance of rain')
     }
     })
 
